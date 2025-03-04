89205805bed888329e8e92c764ed7b160f2bbb45
Neal Daringer <207518+ndaringer@users.noreply.github.com>
2025-03-04 14:46:51 -0500

    Update ip_v6.py

--- ip_v6.py
+++ ip_v6.py
@@ -1,16 +1,26 @@
+import re
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+def is_ipv6(ip):
+    ipv6_pattern = r"^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$"
+    return re.match(ipv6_pattern, ip)
+
 @app.route('/')
 def get_ipv6():
-	ipv6 = request.environ.get('HTTP_X_FORWARDED_FOR') #or request.remote_addr if no proxy
-	if ipv6:
-		ipv6 = ipv6.split(',')[0].strip()
+    ipv6 = None
 
-	return jsonify({
-		'ipv6': ipv6,
-	})
+    xff = request.environ.get('HTTP_X_FORWARDED_FOR')
+    if xff:
+        for ip in xff.split(','):
+            ip = ip.strip()
+            if is_ipv6(ip):
+                if ipv6 is None:
+                    ipv6 = ip
+
+    headers = dict(request.headers)
+    return jsonify({'ipv6': ipv6, 'headers': headers})
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5002) #Different port
+    app.run(host='0.0.0.0', port=5002)
